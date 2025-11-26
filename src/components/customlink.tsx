a45f0a9b8ec221c96468f0215e6748e0891f5a92
Alessandro Guelpa <alexander.guelpa@gmail.com>
2025-11-26 17:08:35 +0100

    modifica link

--- src/components/customlink.tsx
+++ src/components/customlink.tsx
@@ -6,12 +6,12 @@ interface CustomLinkProps {
   className?: string;
   children: React.ReactNode; // Accetta testo, icone o altri elementi React
 }
-const baseClasses = "group relative inline-block text-violet-600 dark:text-violet-500 font-medium cursor-pointer";
+const baseClasses = "group relative inline-block font-medium cursor-pointer";
 const CustomLink = ({ href, children, className}: CustomLinkProps) => {
   return (
     <a 
       href={href} 
-      className={`${baseClasses} ${className || ''}`}
+      className={`${baseClasses} ${className || 'text-violet-600 dark:text-violet-500'}`}
     >
       {children}
       <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-violet-600 transition-all duration-300 ease-in-out group-hover:w-full"></span>
